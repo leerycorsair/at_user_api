9b28c4187ae74a366583ef12f87c01682456e5dd
grigadnal625 <grigandal625@gmail.com>
2024-10-19 13:52:03 +0300

    frontend added

--- AT_USER/src/api/api.py
+++ AT_USER/src/api/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, FastAPI
-
-
+from fastapi.staticfiles import StaticFiles
+from .frontend import frontend_router, FRONTEND_BUILD_PATH
 from .auth_router import router as auth_router
+
+import os
 
 _router = APIRouter (
     prefix="/api",
@@ -10,3 +12,6 @@ _router = APIRouter (
 def setup_routes(app: FastAPI):
     _router.include_router(auth_router)
     app.include_router(_router)
+    app.mount("/static", StaticFiles(directory=os.path.join(FRONTEND_BUILD_PATH, "static")), name="static")
+    app.include_router(frontend_router)
+    
--- AT_USER/src/start.sh
+++ AT_USER/src/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Run Alembic migrations
 alembic upgrade head
 
-uvicorn src.application:app --host 0.0.0.0 --port $SERVER_PORT
+# Check if debugpy is installed
+if [ "$DEBUG" = "true" ]; then
+    if ! python -c "import debugpy" &> /dev/null; then
+        echo "debugpy is not installed. Installing debugpy..."
+        pip install debugpy
+    fi
+
+    # Start the FastAPI app with debugpy
+    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn src.application:app --host 0.0.0.0 --port $SERVER_PORT
+else
+    # Start the FastAPI app without debugpy
+    python -Xfrozen_modules=off -m uvicorn src.application:app --host 0.0.0.0 --port $SERVER_PORT
+fi
