d6aacf18b9f202aff01d7a2e13f39c72c0e7228a
leerycorsair <vladleonov21032002@gmail.com>
2025-03-31 14:56:47 +0300

    fix:cli args

--- at_user_api/config/cli_args.py
+++ at_user_api/config/cli_args.py
@@ -2,47 +2,41 @@ import argparse
 import os
 
 
-def parse_db_args(subparsers: argparse._SubParsersAction):
-    db_parser = subparsers.add_parser("db", help="Database configuration.")
-    db_parser.add_argument("--DB_HOST", type=str, help="Database host.")
-    db_parser.add_argument("--DB_PORT", type=int, help="Database port.")
-    db_parser.add_argument("--DB_NAME", type=str, help="Database name.")
-    db_parser.add_argument("--DB_USER", type=str, help="Database user.")
-    db_parser.add_argument("--DB_PASS", type=str, help="Database password.")
-    return db_parser
-
-def parse_rabbitmq_args(subparsers: argparse._SubParsersAction):
-    rabbitmq_parser = subparsers.add_parser("rabbitmq", help="RabbitMQ configuration.")
-    rabbitmq_parser.add_argument("--RABBITMQ_HOST", type=str, help="RabbitMQ host.")
-    rabbitmq_parser.add_argument("--RABBITMQ_PORT", type=int, help="RabbitMQ port.")
-    rabbitmq_parser.add_argument("--RABBITMQ_LOGIN", type=str, help="RabbitMQ login.")
-    rabbitmq_parser.add_argument(
-        "--RABBITMQ_PASSWORD", type=str, help="RabbitMQ password."
-    )
-    rabbitmq_parser.add_argument(
-        "--RABBITMQ_VHOST", type=str, help="RabbitMQ virtualhost."
-    )
-    rabbitmq_parser.add_argument(
-        "--RABBITMQ_SSL", type=bool, help="Use SSL for RabbitMQ."
-    )
-    return rabbitmq_parser
+def parse_db_args(parser: argparse.ArgumentParser):
+    group = parser.add_argument_group("Database Configuration")
+    group.add_argument("--db_host", type=str, help="Database host.")
+    group.add_argument("--db_port", type=int, help="Database port.")
+    group.add_argument("--db_name", type=str, help="Database name.")
+    group.add_argument("--db_user", type=str, help="Database user.")
+    group.add_argument("--db_pass", type=str, help="Database password.")
 
 
-def parse_server_args(subparsers: argparse._SubParsersAction):
-    server_parser = subparsers.add_parser("server", help="Server configuration.")
-    server_parser.add_argument("--SERVER_PORT", type=int, help="Server port.")
-    return server_parser
+def parse_rabbitmq_args(parser: argparse.ArgumentParser):
+    group = parser.add_argument_group("RabbitMQ configuration")
+    group.add_argument("--rabbitmq_host", type=str, help="RabbitMQ host.")
+    group.add_argument("--rabbitmq_port", type=int, help="RabbitMQ port.")
+    group.add_argument("--rabbitmq_login", type=str, help="RabbitMQ login.")
+    group.add_argument("--rabbitmq_password", type=str, help="RabbitMQ password.")
+    group.add_argument("--rabbitmq_vhost", type=str, help="RabbitMQ virtualhost.")
+    group.add_argument("--rabbitmq_ssl", type=bool, help="Use SSL for RabbitMQ.")
+
+
+def parse_server_args(parser: argparse.ArgumentParser):
+    group = parser.add_argument_group("Server configuration")
+    group.add_argument("--server_port", type=int, help="Server port.")
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Main application configuration.")
-    subparsers = parser.add_subparsers(dest="command")
+    parser = argparse.ArgumentParser(
+        description="Main application configuration.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parse_db_args(subparsers)
-    parse_rabbitmq_args(subparsers)
-    parse_server_args(subparsers)
+    parse_db_args(parser)
+    parse_rabbitmq_args(parser)
+    parse_server_args(parser)
     args = parser.parse_args()
-    
+
     for key, value in vars(args).items():
         if value is not None:
             os.environ[key.upper()] = str(value)
