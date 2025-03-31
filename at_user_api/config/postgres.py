ce54e2e64eac8138f920632d3276478129b12bb3
leerycorsair <vladleonov21032002@gmail.com>
2025-03-31 12:02:12 +0300

    fix:postgres cfg

--- at_user_api/config/postgres.py
+++ at_user_api/config/postgres.py
@@ -1,34 +1,26 @@
-import os
-from dataclasses import dataclass
+from functools import lru_cache
 
-@dataclass
-class DatabaseConfig:
-    host: str
-    port: str
-    name: str
-    user: str
-    password: str
-    url: str
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
-class PostgresConfig:
 
+class DatabaseConfig(BaseSettings):
+    host: str = Field(..., alias="DB_HOST")
+    port: int = Field(..., alias="DB_PORT")
+    name: str = Field(..., alias="DB_NAME")
+    user: str = Field(..., alias="DB_USER")
+    password: str = Field(..., alias="DB_PASS")
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+    class Config:
+        extra = "allow"
+
+
+class PostgresStore:
     @classmethod
+    @lru_cache(maxsize=1)
     def get_database_config(cls) -> DatabaseConfig:
-        DB_HOST = os.getenv("DB_HOST", "localhost")
-        DB_PORT = os.getenv("DB_PORT", "5432")
-        DB_NAME = os.getenv("DB_NAME", "postgres")
-        DB_USER = os.getenv("DB_USER", "postgres")
-        DB_PASS = os.getenv("DB_PASS", "password")
-
-        database_url = (
-            f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-        )
-
-        return DatabaseConfig(
-            host=DB_HOST,
-            port=DB_PORT,
-            name=DB_NAME,
-            user=DB_USER,
-            password=DB_PASS,
-            url=database_url
-        )
+        return DatabaseConfig()
